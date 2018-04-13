798b912d699af359c08f39a0e4f48c9404d7f00a
Juan Borrajo Rodriguez <juanborrajo10@gmail.com>
2018-04-13 08:53:01 +0200

    Clase principal con metodo de visualizacion.

---
+++ src/main/java/com/juan/controlmaven/ClasePrincipal.java
@@ -0,0 +1,17 @@
+
+package com.juan.controlmaven;
+
+
+
+/**
+ *
+ * @author Juan Borrajo Rodriguez
+ */
+public class ClasePrincipal {
+
+    
+    public static void main(String[] args) {
+        Funciones.visualizacion();
+    }
+
+}
